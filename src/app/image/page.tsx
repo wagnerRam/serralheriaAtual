9f9d7000ade360ec923c65940c4112a1a6013e2d
wagrnerRam <wagner19ramos@hotmail.com>
2025-11-10 22:37:10 -0300

    adicionando imagem modelo logico

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,12 +1,18 @@
-"use client"
+"use client";
+import Image from "next/image";
 
 export default function ImagePage() {
   return (
-    <main className="min-h-screen p-8 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Galeria de Imagens</h1>
-      <div className="grid grid-cols-3 gap-4">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold mb-6">Modelo Lógico do Banco de Dados</h1>
 
-      </div>
+      <Image
+        src="/image.png"           // 👈 caminho da imagem (dentro de /public)
+        alt="Modelo lógico do banco de dados"
+        width={1000}                       // 👈 largura da imagem
+        height={600}                       // 👈 altura da imagem
+        className="rounded-lg shadow-md border border-gray-300"
+      />
     </main>
   );
-} 
+}
